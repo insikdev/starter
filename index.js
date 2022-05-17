48a739698a7e79bcdd264429a4a60a07fff33178
Russell Park <russell@infrablockchain.com>
2022-05-17 10:53:03 +0900

    Feat: generate Function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-
 import shell from 'shelljs';
 import inquirer from 'inquirer';
-import { manager, moduleList } from './options.js';
+import fs from 'fs';
+import https from 'https';
+import { manager, moduleList } from './src/options.js';
 
 inquirer
   .prompt([
@@ -14,9 +15,26 @@ inquirer
     }
   ])
   .then(({ package_manager }) => {
-    shell.echo('Generate .eslintrc.json & .prettierrc.json');
+    console.log('Installing required modules...');
     const command = `${manager[package_manager]} ${moduleList.join(' ')} -D`;
     shell.exec(command);
-    shell.cp(['airbnb/.eslintrc.json', 'airbnb/.prettierrc.json'], './');
+    generateConfigFile('eslint', 'airbnb');
+    generateConfigFile('prettier');
     shell.echo('\nDone');
   });
+
+function generateConfigFile(key, type = '') {
+  console.log(`Generate .${key}rc.json`);
+  const BASE_URL =
+    'https://raw.githubusercontent.com/insikdev/dev-starter/main';
+  const options = {
+    eslint: `src/eslint/${type}.json`,
+    prettier: '.prettierrc.json'
+  };
+  const requestURL = `${BASE_URL}/${options[key]}`;
+  https.get(requestURL, (res) => {
+    const filePath = fs.createWriteStream(`.${key}rc.json`);
+    res.pipe(filePath);
+    filePath.on('finish', () => filePath.close());
+  });
+}
